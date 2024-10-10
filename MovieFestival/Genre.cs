dc320d0447e5ec47a111ceb0977d141515b05f17
Mladen Velikinac <mladen.velikinac@traceone.com>
2024-10-10 11:23:59 +0200

    Implement Genre class

---
+++ MovieFestival/Genre.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace MovieFestival;
+
+public class Genre
+{
+    public Genre()
+    {
+        
+    }
+    public string GetData()
+    {
+        return "Horor";
+    }
+
+}
+
